3555bd650e09cda2d38dc1c5a09b5651abebb6fb
Javix <s.cambour@gmail.com>
2012-10-02 22:33:59 +0200

    tests for profile page added

--- spec/requests/admin_pages_spec.rb
+++ spec/requests/admin_pages_spec.rb
@@ -1,12 +1,21 @@
 require 'spec_helper'
 
 describe "Admin Pages" do
-   subject { page }
+  subject { page }
 
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
     it { should have_selector('title', text: full_title('Sign up')) }
+  end
+  
+  describe "profile page" do
+    # Code to make a user variable
+    let(:user) { FactoryGirl.create(:admin) }
+    before { visit admin_path(user) }
+
+    it { should have_selector('h1',    text: user.full_name) }
+    it { should have_selector('title', text: user.full_name) }
   end
 end
