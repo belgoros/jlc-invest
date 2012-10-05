0ca8d509b75b1612f5e9b3fab65362b1435c6b08
Javix <s.cambour@gmail.com>
2012-10-05 22:04:18 +0200

    admin tests added

--- spec/requests/admin_pages_spec.rb
+++ spec/requests/admin_pages_spec.rb
@@ -2,13 +2,6 @@ require 'spec_helper'
 
 describe "Admin Pages" do
   subject { page }
-
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
-  end
   
   describe "profile page" do
     # Code to make a user variable
@@ -17,5 +10,38 @@ describe "Admin Pages" do
 
     it { should have_selector('h1',    text: user.full_name) }
     it { should have_selector('title', text: user.full_name) }
+  end  
+
+  describe "signup page" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    it { should have_selector('h1',    text: 'Sign up') }
+    it { should have_selector('title', text: full_title('Sign up')) }
+    
+    describe "with invalid information" do    
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(Admin, :count)
+      end
+    end
+    
+    describe "with valid information" do
+      before do
+        fill_in "Firstname",with: "Jean"
+        fill_in "Lastname", with: "Dupont"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+
+      it "should create a user" do
+        expect { click_button submit }.to change(Admin, :count).by(1)
+      end
+    end
+    
+    
   end
+  
+  
+    
 end
