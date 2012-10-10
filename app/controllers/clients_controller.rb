cb6d55b47d31240e83dcb268399ffa9a7df46e2f
Javix <s.cambour@gmail.com>
2012-10-10 20:37:50 +0200

    create action added

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,6 +8,16 @@ class ClientsController < ApplicationController
   def new
     @client = Client.new
   end
+  
+  def create
+    @client = Client.new(params[:client])
+    if @client.save      
+      flash[:success] = "Client created with success"
+      redirect_to clients_path
+    else
+      render "new"
+    end
+  end
 
   def edit
     @client = Client.find(params[:id])
