f7b087f23fc0ff6183f17f9bd6df76b4c284f282
Javix <s.cambour@gmail.com>
2012-10-14 22:30:24 +0200

    code formatted

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,24 +1,24 @@
 class Client < ActiveRecord::Base
   has_many :operations, dependent: :destroy
-  attr_accessible :firstname, :lastname,:street, :house, :box, :zipcode, :city, :country, :phone
-  
-  validates :firstname, presence: true, length: { maximum: 50 }
-  validates :lastname, presence: true, length: { maximum: 50 }, uniqueness: { case_sensitive: false, scope: "firstname"}
-  validates :street, presence: true, length: { maximum: 50 }
-  validates :house, presence: true, length: { maximum: 5 }
-  validates :zipcode, presence: true, length: { maximum: 5 }
-  validates :city, presence: true, length: { maximum: 50 }
-  validates :country, presence: true, length: { maximum: 50 }
-  
+  attr_accessible :firstname, :lastname, :street, :house, :box, :zipcode, :city, :country, :phone
+
+  validates :firstname, presence: true, length: {maximum: 50}
+  validates :lastname, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false, scope: "firstname"}
+  validates :street, presence: true, length: {maximum: 50}
+  validates :house, presence: true, length: {maximum: 5}
+  validates :zipcode, presence: true, length: {maximum: 5}
+  validates :city, presence: true, length: {maximum: 50}
+  validates :country, presence: true, length: {maximum: 50}
+
   before_validation do |client|
     client.firstname = firstname.capitalize.strip
     client.lastname = lastname.upcase.strip
-    client.street = street.capitalize.strip        
+    client.street = street.capitalize.strip
     client.zipcode = zipcode.strip
     client.city = city.capitalize.strip
-    client.country = country.upcase.strip    
+    client.country = country.upcase.strip
   end
-  
+
   def full_name
     ["#{firstname}", "#{lastname}"].join(' ')
   end
