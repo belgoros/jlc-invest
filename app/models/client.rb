7ad898242b1276e2f62bd3762e18578cf06dfb52
Javix <s.cambour@gmail.com>
2012-10-16 23:36:37 +0200

    zipcode length validation changed

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,7 +6,7 @@ class Client < ActiveRecord::Base
   validates :lastname, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false, scope: "firstname"}
   validates :street, presence: true, length: {maximum: 50}
   validates :house, presence: true, length: {maximum: 5}
-  validates :zipcode, presence: true, length: {maximum: 5}
+  validates :zipcode, presence: true, length: {maximum: 15}
   validates :city, presence: true, length: {maximum: 50}
   validates :country, presence: true, length: {maximum: 50}
 
