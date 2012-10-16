326c0c0ebd43aab404d9a04a8385e50cd5c1c9ab
Javix <s.cambour@gmail.com>
2012-10-16 23:35:44 +0200

    operations controller added

---
+++ app/controllers/operations_controller.rb
@@ -0,0 +1,52 @@
+class OperationsController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :new, :edit, :update, :destroy]
+  before_filter :find_client, except: :index
+  
+  def index
+    @operations = Operation.paginate(page: params[:page])    
+  end  
+  
+  def new
+    @operation = @client.operations.new
+  end
+  
+  def create
+    @operation = @client.operations.new(params[:operation])    
+    if @operation.save
+      flash[:success] = "Operation created with success"
+      redirect_to @client
+    else
+      render "new"
+    end    
+  end  
+  
+  def edit
+    @operation = @client.operations.find(params[:id])
+  end
+  
+  def show
+    @operation = @client.operations.find(params[:id])
+  end
+
+  def update
+    @operation = @client.operations.find(params[:id])
+    if @operation.update_attributes(params[:operation])
+      flash[:success] = "Operation updated"
+      redirect_to @client
+    else
+      render 'edit'      
+    end    
+  end
+
+  def destroy
+    @client.operations.find(params[:id]).destroy
+    flash[:success] = "Operation destroyed."
+    redirect_to @client
+  end
+  
+  
+  private
+  def find_client
+    @client = Client.find(params[:client_id])
+  end
+end
