da9b1ddd323cd2be5aacc07f51b83ce81c5b6ed8
Javix <s.cambour@gmail.com>
2012-10-16 23:33:51 +0200

    show: collection of operations added

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,5 @@
 class ClientsController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :new, :edit, :update, :destroy]
+  before_filter :signed_in_user, only: [:index, :new, :edit, :update, :destroy]  
 
   def index
     @clients = Client.paginate(page: params[:page])
@@ -25,6 +25,7 @@ class ClientsController < ApplicationController
 
   def show
     @client = Client.find(params[:id])
+    @operations = @client.operations.paginate(page: params[:page])    
   end
 
   def update
