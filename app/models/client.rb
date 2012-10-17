9f0ca1549992d62e0b08ed0190c57289cc70db6b
Javix <s.cambour@gmail.com>
2012-10-17 22:02:28 +0200

    default scope added, first name splitted

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,17 +1,18 @@
 class Client < ActiveRecord::Base
   has_many :operations, dependent: :destroy
+  default_scope order: 'clients.lastname'
   attr_accessible :firstname, :lastname, :street, :house, :box, :zipcode, :city, :country, :phone
 
   validates :firstname, presence: true, length: {maximum: 50}
   validates :lastname, presence: true, length: {maximum: 50}, uniqueness: {case_sensitive: false, scope: "firstname"}
   validates :street, presence: true, length: {maximum: 50}
   validates :house, presence: true, length: {maximum: 5}
-  validates :zipcode, presence: true, length: {maximum: 15}
+  validates :zipcode, presence: true, length: {maximum: 5}
   validates :city, presence: true, length: {maximum: 50}
   validates :country, presence: true, length: {maximum: 50}
 
   before_validation do |client|
-    client.firstname = firstname.capitalize.strip
+    client.firstname = firstname.strip.split('-').map(&:capitalize).join('-')
     client.lastname = lastname.upcase.strip
     client.street = street.capitalize.strip
     client.zipcode = zipcode.strip
