cdbd1ac5dc8359a8781ce040763192a235af3e18
Javix <s.cambour@gmail.com>
2012-10-18 23:10:27 +0200

    inheritance added for operations

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,30 +8,40 @@ FactoryGirl.define do
   end
 
   factory :client do
-    sequence(:firstname) { |n| "firstname #{n}" }
-    sequence(:lastname) { |n| "lastname #{n}" }
-    sequence(:street) { |n| "street #{n}" }
+    sequence(:firstname) { |n| "firstname_#{n}" }
+    sequence(:lastname) { |n| "lastname_#{n}" }
+    sequence(:street) { |n| "street_#{n}" }
     sequence(:house) { |n| "1#{n}" }
     sequence(:zipcode) { |n| "1#{n}" }
-    sequence(:city) { |n| "city #{n}" }
-    sequence(:country) { |n| "country #{n}" }
+    sequence(:city) { |n| "city_#{n}" }
+    sequence(:country) { |n| "country_#{n}" }
   end
 
   factory :operation do
-    operation_type Operation::TRANSACTIONS[0]
+    value_date Date.today
     sum 1000
     duration 6
     rate 12
-    value_date Date.today
+    close_date Date.today + 6.months
     client
-  end
-
-  factory :withdrawal, class: Operation do
-    operation_type Operation::TRANSACTIONS[1]
-    sum 1000
-    value_date Date.today
-    client
-  end
+    
+    factory :deposit do
+      operation_type Operation::TRANSACTIONS[0]      
+    end
+    
+    factory :remission do
+      operation_type Operation::TRANSACTIONS[2]
+    end
+    
+    factory :withdrawal do
+      operation_type Operation::TRANSACTIONS[1]
+      sum 1000
+      duration nil
+      rate nil
+    end
+    
+    
+  end 
 
 end
 
