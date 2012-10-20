e2ec8f9efbc44727c4e2b0fc692d1ada4b7390fb
Javix <s.cambour@gmail.com>
2012-10-21 00:25:10 +0200

    factory call syntax changed, test description modified

--- spec/requests/operation_pages_spec.rb
+++ spec/requests/operation_pages_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-describe "Client Operation Pages" do
+describe "Operation Pages" do
   subject { page }
   
-  let(:admin) { FactoryGirl.create(:admin) }
-  let(:client) { FactoryGirl.create(:client) }  
+  let(:admin) { create(:admin) }
+  let(:client) { create(:client) }
 
   before(:each) { sign_in admin }
   
@@ -15,19 +15,26 @@ describe "Client Operation Pages" do
     it { should have_selector('h1', text: 'All operations') }
 
     
-#    before(:all) { 31.times { FactoryGirl.create(:operation, client: client) } }
-#    after(:all) { client.operations.delete_all }
-#
-#    describe "pagination" do
-#
-#      it { should have_selector('div.pagination') }
-#
-#      it "should list each operation" do
-#        Operation.paginate(page: 1).each do |operation|
-#          page.should have_selector('td', text: operation.value_date.to_s)
-#        end
-#      end
-#    end
+=begin
+    before(:all) do
+      35.times do
+        create(:deposit, client: client, value_date: Date.today, close_date: Date.today + 3.months,
+               sum: 1000,rate:1.25)
+      end
+    end
+    after(:all) { client.operations.delete_all }
+
+    describe "pagination" do
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each operation" do
+        Operation.paginate(page: 1).each do |operation|
+          page.should have_selector('td', text: operation.value_date.to_s)
+        end
+      end
+    end
+=end
   end
   
 end
