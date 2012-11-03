626d2fd59207c0f55825412fac5a83606a39e5de
Javix <s.cambour@gmail.com>
2012-11-03 21:06:36 +0100

    tests modified

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -2,7 +2,23 @@ require 'spec_helper'
 
 describe Account do
   let(:client) { create(:client) }
-  subject { build(:account, client: client) }
+  before { @account = build(:account, client: client) }
 
-  it { should be_valid}
+  subject { @account }
+  
+  its(:client) { should == client }
+
+  it { should be_valid }
+  
+  it { should respond_to(:acc_number) }
+  it { should respond_to(:client_id) }
+  it { should respond_to(:client) }
+  it { should respond_to(:operations) } 
+  
+
+  describe "has a generated number" do
+    before { @account.save }    
+    its(:acc_number) { should_not be_nil }
+  end  
+  
 end
