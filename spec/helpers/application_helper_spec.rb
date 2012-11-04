a796a8b4a2f3328cb513fb36e94c2b0f7d667f73
Javix <s.cambour@gmail.com>
2012-11-04 16:22:05 +0100

    translation added

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,7 +8,8 @@ describe ApplicationHelper do
     end
 
     it "should include the base title" do
-      full_title("foo").should =~ /^JLC Investment/
+      base_title = I18n.t(:base_title)
+      full_title("foo").should =~ /^#{base_title}/
     end
 
     it "should not include a bar for the home page" do
