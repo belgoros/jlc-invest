efa2a6d850b195a629f82300cfd95c3c83c024df
Javix <s.cambour@gmail.com>
2012-11-17 10:04:33 +0100

    changes balance calculation

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,7 @@ class Account < ActiveRecord::Base
   attr_accessible :acc_number, :client_id
   belongs_to :client
   has_many :operations, dependent: :destroy
-  scope :operations_by_client, joins(:client, :operations).select('clients.firstname, clients.lastname, sum(operations.total) as total').group('clients.id, clients.firstname, clients.lastname').order('clients.lastname')
+  scope :operations_by_client, joins(:client, :operations).select('clients.firstname, clients.lastname, sum(operations.total) as total').group('clients.id, clients.firstname, clients.lastname').order('clients.lastname')  
 
   SEPARATOR = '-'
 
@@ -10,17 +10,31 @@ class Account < ActiveRecord::Base
 
   before_save :generate_account_number
   
-  def balance
-    operations.map(&:total).inject(:+)
-  end
+  def balance                
+    except_remissions = deposits_and_withdrawals(operations)    
+    sum_except_remissions = except_remissions.map(&:total).inject(:+) || 0    
+    remissions_operations = remissions(operations)
+    sum_except_remissions += remissions_operations.map(&:interests).inject(:+) unless remissions_operations.empty?    
+    sum_except_remissions
+  end 
+  
+  
 
   private
 
-    def generate_account_number
-      last_found = Account.last
-      year_prefix = Date.today.year.to_s
-      acc_suffix = 0
-      acc_suffix = last_found.acc_number.split(SEPARATOR).last.to_i if last_found
-      self.acc_number = year_prefix << SEPARATOR << sprintf("%05d", acc_suffix + 1)
-    end
+  def generate_account_number
+    last_found = Account.last
+    year_prefix = Date.today.year.to_s
+    acc_suffix = 0
+    acc_suffix = last_found.acc_number.split(SEPARATOR).last.to_i if last_found
+    self.acc_number = year_prefix << SEPARATOR << sprintf("%05d", acc_suffix + 1)
+  end
+  
+  def deposits_and_withdrawals(items)
+    items.select {|op| op.operation_type != Operation::REMISSION }    
+  end  
+  
+  def remissions(items)
+    items.select {|op| op.operation_type == Operation::REMISSION }    
+  end
 end
