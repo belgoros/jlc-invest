6263462fcef125f80d9218283fc4c8041217cd9e
Javix <s.cambour@gmail.com>
2014-03-18 20:55:24 +0100

    Update/add config files to Rails4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-JlcInvest::Application.config.secret_token = 'b9e2dc845863473b036144d53dbd9c4d9176e8099b453236209a3e893dfe9e4d2a352d2664ec2deb1bdf1e5145f307a27af311be9f155b4326dff83ce70cf908'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JlcInvest::Application.config.secret_key_base = '252b999eac7f8fd80b7be300faf6c3c6bd08e4966fc31c355132d4657b9c1cd483923832ac29d8e6a3e05e1607d2bc9285c2749c4b240516b30712ae38ea9b76'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-JlcInvest::Application.config.session_store :cookie_store, key: '_jlc-invest_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# JlcInvest::Application.config.session_store :active_record_store
+JlcInvest::Application.config.session_store :cookie_store, key: '_jlc_invest_session'
