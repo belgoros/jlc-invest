71f554c14abffa33459bd70a10f09d48a7a9e5e1
Javix <s.cambour@gmail.com>
2014-04-03 21:27:20 +0200

    Downcase email from params

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = Admin.find_by_email(params[:session][:email])
+    user = Admin.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
       redirect_back_or user
