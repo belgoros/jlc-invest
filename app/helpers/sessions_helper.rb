89ca3664bdf978916ccdf52eaf491a0f3668fc66
Javix <s.cambour@gmail.com>
2014-04-03 21:28:45 +0200

    Modify authentication method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,10 @@
 module SessionsHelper
 
   def sign_in(user)
-    cookies.permanent[:remember_token] = user.remember_token
-    current_user = user
+    remember_token = Admin.new_remember_token
+    cookies.permanent[:remember_token] = remember_token
+    user.update_attribute(:remember_token, Admin.hash(remember_token))
+    self.current_user = user
   end
 
   def signed_in?
@@ -14,7 +16,8 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= Admin.find_by_remember_token(cookies[:remember_token])
+    remember_token  = Admin.hash(cookies[:remember_token])
+    @current_user ||= Admin.find_by(remember_token: remember_token)
   end
 
   def current_user?(user)
@@ -22,8 +25,9 @@ module SessionsHelper
   end
 
   def sign_out
-    current_user = nil
+    current_user.update_attribute(:remember_token, Admin.hash(Admin.new_remember_token))
     cookies.delete(:remember_token)
+    self.current_user = nil
   end
 
   def store_location
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,8 +2,8 @@ class Admin < ActiveRecord::Base
 
   has_secure_password
 
-  validates :firstname, presence: true, length: {maximum: 50}
-  validates :lastname, presence: true, length: {maximum: 50}
+  validates :firstname, presence: true, length: { maximum: 50 }
+  validates :lastname,  presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
@@ -11,24 +11,34 @@ class Admin < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  before_save do |user|
-    user.email     = email.downcase.strip
-    user.firstname = firstname.strip.split('-').map(&:capitalize).join('-')
-    user.lastname  = lastname.upcase.strip
+  before_save :normalize_admin_identity
+  before_create :create_remember_token
+
+  def self.new_remember_token
+    SecureRandom.urlsafe_base64
   end
 
-  before_save :create_remember_token
-
+  def self.hash(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 
   def full_name
     ["#{firstname}", "#{lastname}"].join(' ')
   end
 
+  def normalize_admin_identity
+    self.email     = email.downcase.strip
+    self.firstname = firstname.strip.split('-').map(&:capitalize).join('-')
+    self.lastname  = lastname.upcase.strip
+  end
+
+  protected :normalize_admin_identity
+
 
   private
 
   def create_remember_token
-    self.remember_token = SecureRandom.urlsafe_base64
+    self.remember_token = Admin.hash(Admin.new_remember_token)
   end
 
 end
