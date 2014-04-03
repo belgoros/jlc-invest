00057cf3eebfa3dde1583c445738decfdecda317
Javix <s.cambour@gmail.com>
2014-04-03 21:29:32 +0200

    Add rspec, factory_girl to config generators

--- config/application.rb
+++ config/application.rb
@@ -9,5 +9,17 @@ module JlcInvest
     config.autoload_paths += %W(#{Rails.root}/lib)
     I18n.config.enforce_available_locales = true
     config.i18n.default_locale = :fr
+
+    config.generators do|g|
+      g.test_framework :rspec,
+        fixtures:         true,
+        view_specs:       false,
+        helper_specs:     false,
+        routing_specs:    false,
+        controller_specs: true,
+        request_specs:    false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
+
   end
 end
