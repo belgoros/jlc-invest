5126b6372a0f30fbe61a2a8e3987b62e8115c192
Javix <s.cambour@gmail.com>
2014-05-30 17:30:33 +0200

    Add LoginMacros to enable sign_in with Capybara

---
+++ spec/support/login_macros.rb
@@ -0,0 +1,8 @@
+module LoginMacros
+  def sign_in_with_browser(user)
+    visit signin_path
+    fill_in I18n.t('activerecord.attributes.admin.email'), with: user.email
+    fill_in I18n.t('activerecord.attributes.admin.password'), with: user.password
+    click_button I18n.t('links.sign_in')
+  end
+end
