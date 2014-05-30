6e5903c56b2876a8a26efbd459004c6c3bd42f01
Javix <s.cambour@gmail.com>
2014-05-30 12:07:26 +0200

    Remove sign_in method, include SessionsHelper

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,11 +1,3 @@
 include ApplicationHelper
 include ActionView::Helpers::NumberHelper
-
-def sign_in(user)
-  visit signin_path
-  fill_in I18n.t('activerecord.attributes.admin.email'), with: user.email
-  fill_in I18n.t('activerecord.attributes.admin.password'), with: user.password
-  click_button I18n.t('links.sign_in')
-  # Sign in when not using Capybara as well.
-  cookies[:remember_token] = user.remember_token
-end
+include SessionsHelper
